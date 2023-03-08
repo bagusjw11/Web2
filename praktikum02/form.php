a7099a31e0ad49716e90940c11c15e70b48a7edc
Bagus Kurniawan <114548566+bagusjw11@users.noreply.github.com>
2023-03-08 12:00:39 +0700

    Add files via upload

---
+++ praktikum02/form.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+    <div class="container">
+        <hi>From Nilai Siswa</hi>
+        <form method="POST" action="nilai_siswa.php">
+            <div class="form-group row">
+                <label for="nama" class="col-4 col-form-label">Nama Lengkap</label>
+                <div class="col-8">
+                    <input id="nama" name="nama" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label>
+                <div class="col-8">
+                    <select id="matkul" name="matkul" class="custom-select">
+                        <option value="Pemograman Web">Web</option>
+                        <option value="Dasar Dasar Pemograman">DDP</option>
+                        <option value="Sistem Operasi">SO</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label>
+                <div class="col-8">
+                    <input id="nilai_uts" name="nilai_uts" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai _uas" class="col-4 col-form-label">Nilai UAS</label>
+                <div class="col-8">
+                    <input id="nilai_uas" name="nilai_uas" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai-tugas" class="col-4 col-form-label">Nilai Tugas</label>
+                <div class="col-8">
+                    <input id="nilai_tugas" name="nilai_tugas" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
